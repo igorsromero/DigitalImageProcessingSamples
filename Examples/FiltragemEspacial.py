bb63de2deadae9348e81a1d068e373c8ba64f0b4
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-09-15 19:39:40 -0300

    Atualizando FiltragemEspacial

--- Examples/FiltragemEspacial.py
+++ Examples/FiltragemEspacial.py
@@ -1,109 +1,25 @@
 import numpy as np
+from numpy import asarray
 from PIL import Image
-from matplotlib import pyplot as plt
 from scipy import ndimage
 
-# Seleciona a imagem
-image = Image.open('Images/imagem4.jpg')
-# image.show()
-# Imagem para array
-img_array = np.array(image).astype(int)
+  
+def main():
+    # Open image
+    image_in = Image.open('images/imagem4.jpg')
+    # convert image to numpy array    
+    # convert to int to allow negative values
+    image_np = np.array(image_in).astype(int) 
+    # Convolution filter usingo mode reflect
+    kernel = np.array([[ 1, 0,-1],
+                       [ 0, 0, 0],
+                       [-1, 0, 1]])  
+    im1 = ndimage.convolve(image_np, kernel, mode='reflect')
+    # replace negative values with 0
+    im1 = np.where(im1<0, 0, im1)
+    # convert array to image usint uint8 type
+    image_out = Image.fromarray(im1.astype('uint8'))   
+    image_out.save('images/A.jpg')
 
-
-## ALTERNATIVA A
-
-# Adicionando os pesos
-k = np.array([[0, 0, 0],[0, 1, 0],[0, 0, 0]])
-# Realiza a convolução e salva a imagem
-ndarray = ndimage.convolve(img_array, k, mode='wrap', cval=0.0)
-img_array_convolved = np.array(ndarray)
-img_convolved = Image.fromarray(img_array_convolved)
-img_convolved.convert('L').save('Images/FiltragemEspacial/A.jpg')
-
-## ALTERNATIVA B
-
-# Adicionando os pesos
-k = np.array([[1, 0, -1],[0, 0, 0],[-1, 0, 1]])
-# Realiza a convolução e salva a imagem
-ndarray = ndimage.convolve(img_array, k, mode='wrap', cval=0.0)
-img_array_convolved = np.array(ndarray)
-img_convolved = Image.fromarray(img_array_convolved)
-img_convolved.convert('L').save('Images/FiltragemEspacial/B.jpg')
-
-## ALTERNATIVA C
-
-# Adicionando os pesos
-k = np.array([[0, -1, 0],[-1, 4, -1],[0, -1, 0]])
-# Realiza a convolução e salva a imagem
-ndarray = ndimage.convolve(img_array, k, mode='wrap', cval=0.0)
-img_array_convolved = np.array(ndarray)
-img_convolved = Image.fromarray(img_array_convolved)
-img_convolved.convert('L').save('Images/FiltragemEspacial/C.jpg')
-
-## ALTERNATIVA D
-
-# Adicionando os pesos
-k = np.array([[-1, -1, -1],[-1, 8, -1],[-1, -1, -1]])
-# Realiza a convolução e salva a imagem
-ndarray = ndimage.convolve(img_array, k, mode='wrap', cval=0.0)
-img_array_convolved = np.array(ndarray)
-img_convolved = Image.fromarray(img_array_convolved)
-img_convolved.convert('L').save('Images/FiltragemEspacial/D.jpg')
-
-## ALTERNATIVA E
-
-# Adicionando os pesos
-k = np.array([[0, -1, 0],[-1, 5, -1],[0, -1, 0]])
-# Realiza a convolução e salva a imagem
-ndarray = ndimage.convolve(img_array, k, mode='wrap', cval=0.0)
-img_array_convolved = np.array(ndarray)
-img_convolved = Image.fromarray(img_array_convolved)
-img_convolved.convert('L').save('Images/FiltragemEspacial/E.jpg')
-
-## ALTERNATIVA F
-
-# Adicionando os pesos
-k = np.array([[1, 1, 1],[1, 1, 1],[1, 1, 1]])
-#Multiplicação pela constante caso necessário
-m = k*(1/9)
-# Realiza a convolução e salva a imagem
-ndarray = ndimage.convolve(img_array, m, mode='wrap', cval=0.0)
-img_array_convolved = np.array(ndarray)
-img_convolved = Image.fromarray(img_array_convolved)
-img_convolved.convert('L').save('Images/FiltragemEspacial/F.jpg')
-
-## ALTERNATIVA G
-
-# Adicionando os pesos
-k = np.array([[1, 2, 1],[2, 4, 2],[1, 2, 1]])
-#Multiplicação pela constante caso necessário
-m = k*(1/16)
-# Realiza a convolução e salva a imagem
-ndarray = ndimage.convolve(img_array, m, mode='wrap', cval=0.0)
-img_array_convolved = np.array(ndarray)
-img_convolved = Image.fromarray(img_array_convolved)
-img_convolved.convert('L').save('Images/FiltragemEspacial/G.jpg')
-
-## ALTERNATIVA H
-
-# Adicionando os pesos
-k = np.array([[1, 4, 6, 4, 1],[4, 16, 24, 16, 4],[6, 24, 36, 24, 6],[4, 16, 24, 16, 4],[1, 4, 6, 4, 1]])
-#Multiplicação pela constante caso necessário
-m = k*(1/256)
-# Realiza a convolução e salva a imagem
-ndarray = ndimage.convolve(img_array, m, mode='wrap', cval=0.0)
-img_array_convolved = np.array(ndarray)
-img_convolved = Image.fromarray(img_array_convolved)
-img_convolved.convert('L').save('Images/FiltragemEspacial/H.jpg')
-
-## ALTERNATIVA I
-
-# Adicionando os pesos
-k = np.array([[1, 4, 6, 4, 1],[4, 16, 24, 16, 4],[6, 24, -476, 24, 6],[4, 16, 24, 16, 4],[1, 4, 6, 4, 1]])
-#Multiplicação pela constante caso necessário
-m = k*(-1/256)
-# Realiza a convolução e salva a imagem
-ndarray = ndimage.convolve(img_array, m, mode='wrap', cval=0.0)
-img_array_convolved = np.array(ndarray)
-img_convolved = Image.fromarray(img_array_convolved)
-img_convolved.convert('L').save('Images/FiltragemEspacial/I.jpg')
+if __name__ == "__main__":
+    main()
